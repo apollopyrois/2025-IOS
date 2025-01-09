28c4b0ed3cba0122a3aa081125305b2cf4471f41
Liam MacDonald <126014967+apollopyrois@users.noreply.github.com>
2025-01-09 12:06:02 -0400

    Jan 9th

---
+++ MyTime/MyTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MyTime
+//
+//  Created by MacDonald,Liam on 2025-01-09.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        int minutes = 0;
+        NSLog(@"Enter an amount of time in minutes");
+        scanf("%i", &minutes);
+        
+        
+        
+        fflush(stdin);
+    }
+    return 0;
+}
--- ObjCObjects/ObjCObjects/main.m
+++ ObjCObjects/ObjCObjects/main.m
@@ -7,14 +7,76 @@
 
 #import <Foundation/Foundation.h>
 
-@interface <#class name#> : <#superclass#>
+@interface DMFraction : NSObject{
+@private
+    int _numerator;
+    int _denominator;
+}
+
+//Setters
+- (void) setNumerator:(int) numerator;
+- (void) setDenominator:(int) denominator;
+- (void) setNumeratorAndDenominator:(int) numerator withDenominator:(int) denominator;
+
+
+//Getters
+-(int) numerator;
+-(int) denominator;
+
+//Methods
+- (void) print;
+
+@end
+
+@implementation DMFraction
+
+//Setters
+- (void) setNumerator:(int) numerator {
+    _numerator = numerator;
+}
+
+- (void) setDenominator:(int)denominator {
+    _denominator = denominator;
+}
+
+-(void) setNumeratorAndDenominator: (int) numerator withDenominator:(int) denominator{
+    _numerator = numerator;
+    _denominator = denominator;
+}
+//Getters
+- (int) numerator{
+    return _numerator;
+}
+
+- (int) denominator {
+    return _denominator;
+}
+
+//Methods
+- (void) print{
+    NSLog(@"The Result of %i divided by %i is: %g",_numerator,_denominator,(double)_numerator / (double)_denominator);
+}
+
+- (NSString*) description {
+    NSString *fracDesc = [NSString stringWithFormat:@"Numerator: %i, Denominator: %i", _numerator, _denominator];
+    return fracDesc;
+}
 
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        DMFraction *fract = [[DMFraction alloc]init];
+        [fract setNumerator:1];
+        [fract setDenominator:3];
+        [fract print];
+        
+        //Calc 2
+        [fract setNumeratorAndDenominator:10 withDenominator:5];
+        [fract print];
+        
+        NSLog(@"Your fraction Object: %@", fract);
+        
     }
     return 0;
 }
