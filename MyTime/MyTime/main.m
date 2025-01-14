287dc234e352b6f42f7502804c05f72d4fd5f205
Liam MacDonald <126014967+apollopyrois@users.noreply.github.com>
2025-01-14 10:19:44 -0400

    Jan 14th

--- MyTime/MyTime/main.m
+++ MyTime/MyTime/main.m
@@ -7,6 +7,19 @@
 
 #import <Foundation/Foundation.h>
 
+@interface TimeConvert : NSObject{
+@private
+    int _minutes;
+}
+@end
+
+@implementation TimeConvert
+- (NSString) convertToHours:(int) minutes{
+    minutes += 60;
+    return minutes;
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
@@ -14,6 +27,10 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Enter an amount of time in minutes");
         scanf("%i", &minutes);
         
+        TimeConvert *timeConvert = [[TimeConvert alloc]init];
+        
+        minutes = [timeConvert convertToHours:minutes];
+        NSLog(@"%i", minutes);
         
         
         fflush(stdin);
